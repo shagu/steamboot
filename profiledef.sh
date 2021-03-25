079dbb34a5e910aadd46993b8f6a5c4082de59b8
shagu <ericmauser@googlemail.com>
2021-03-25 16:21:40 +0100

    steamboot: add distro names

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux-baseline"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux baseline"
+iso_name="steamboot"
+iso_label="STEAMBOOT_$(date +%Y%m)"
+iso_publisher="steamboot <https://archlinux.org>"
+iso_application="steamboot live"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
